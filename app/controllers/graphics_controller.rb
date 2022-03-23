42cab1196aabba535f238ff3406e316f3e56ce43
IsraelSantos7792 <israelqueren1805@gmail.com>
2022-03-23 12:02:25 -0300

    refactoring: view graphs and controller graphs, update

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -1,4 +1,20 @@
 class GraphicsController < ApplicationController
+  before_action :set_todo_item
+  before_action :set_todo_list
+
   def index
+    @column_chart = @todo_item.group_by_hour_of_day(:created_at, format: "%l %P").count
+    @pie_chart = @todo_item.group(:completed).count
+    @line_chart = @todo_list.group_by_day(:created_at).count
+  end
+
+  private
+
+  def set_todo_item
+    @todo_item = TodoItem.all
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.all
   end
 end
